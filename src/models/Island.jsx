7c0a795e094f61a28b22ab65b5fcb7944bcf5428
IkokuDev <vellidevv@gmail.com>
2024-01-12 13:48:32 +0100

    minor revisions

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -35,21 +35,20 @@ const Island = ({isRotating, setIsRotating, ...props})=> {
     e.preventDefault();
     setIsRotating(false);
 
-    
-  }
-  const handlePointerMove = (e)=>{
-    e.stopPropagation();
-    e.preventDefault();
-
-    if(isRotating){
-      const clientX = e.touches ? e.touches[0].clientX : e.clientX
+    const clientX = e.touches ? e.touches[0].clientX : e.clientX
       const delta = (clientX - lastX.current) / viewport.width
 
       islandRef.current.rotation.y += delta * 0.01 * Math.PI
       lastX.current = clientX
 
       rotationSpeed.current = delta * 0.01 * Math.PI
-    }
+    
+  }
+  const handlePointerMove = (e)=>{
+    e.stopPropagation();
+    e.preventDefault();
+
+    if(isRotating) handlePointerUp(e)
   }
 
   const handleKeyDown = (e)=>{
